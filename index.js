f99e6edbe1815c473b0dc64f725380bcf3d77637
Seonhyuk <gomduridkstjsgur@gmail.com>
2022-09-05 09:36:56 +0900

    second commit

--- index.js
+++ index.js
@@ -10,36 +10,65 @@ for (const operator of operators) {
   const name = document.createElement("h1");
   oper_box.appendChild(name);
   let num = data.operatorCodes[operator.opcodeIndex].deprecatedBuiltinCode;
-  if (num >= 0) {
-    name.innerText = BuiltinOperator[num];
-  } else {
-    name.innerText = BuiltinOperator[String(num)];
-  }
+  let opName;
 
+  if (num >= 0) {
+    opName = BuiltinOperator[num];
+  } else {
+    opName = BuiltinOperator[String(num)];
+  }
+  name.innerText = opName;
+
+  const input_box = document.createElement("div");
   const input = document.createElement("h2");
   const output = document.createElement("h2");
   input.innerText = "Inputs";
   output.innerText = "Outputs";
 
-  oper_box.appendChild(input);
-  for (i of operator.inputs) {
-    const tensor_box = document.createElement("div");
-    const tensor_name = document.createElement("p");
-    tensor_name.innerText = tensors[i].name;
-    tensor_box.appendChild(tensor_name);
-    oper_box.appendChild(tensor_box);
+  let input_info;
+  for (meta of metadata) {
+    if (opName.toLowerCase().replace(/\_/g, "") === meta.name.toLowerCase()) {
+      input_info = meta.inputs;
+      break;
+    }
   }
-  const br = document.createElement("br");
-  oper_box.appendChild(br);
 
-  oper_box.appendChild(output);
-  for (o of operator.outputs) {
-    const tensor_box = document.createElement("div");
-    const tensor_name = document.createElement("p");
-    tensor_name.innerText = tensors[o].name;
-    tensor_box.appendChild(tensor_name);
-    oper_box.appendChild(tensor_box);
+  for (i in input_info) {
+    if (input_info[i].name !== "input") {
+      const tensor_box = document.createElement("div");
+      const tensor_name = document.createElement("p");
+      const tensor_shape = tensors[operator.inputs[i]].shape;
+      tensor_name.innerText = input_info[i].name + `<${tensor_shape.join("×")}>`;
+      tensor_box.appendChild(tensor_name);
+      input_box.appendChild(tensor_box);
+    }
   }
+
+  if (input_box.childElementCount > 0) {
+    oper_box.appendChild(input);
+    oper_box.appendChild(input_box);
+    const br = document.createElement("br");
+    oper_box.appendChild(br);
+  }
+
+  // oper_box.appendChild(output);
+
+  // let output_info;
+  // for (meta of metadata) {
+  //   if (opName.toLowerCase().replace(/\_/g, "") === meta.name.toLowerCase()) {
+  //     output_info = meta.outputs;
+  //     break;
+  //   }
+  // }
+
+  // for (o of output_info) {
+  //   const tensor_box = document.createElement("div");
+  //   const tensor_name = document.createElement("p");
+  //   tensor_name.innerText = o.name;
+  //   tensor_box.appendChild(tensor_name);
+  //   oper_box.appendChild(tensor_box);
+  // }
+
   main.appendChild(oper_box);
   const hr = document.createElement("hr");
   main.appendChild(hr);
